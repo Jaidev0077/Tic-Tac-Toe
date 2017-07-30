6b2407622f11f17d733ba8fa570028507a43de8d
Jaidev0077 <30481097+Jaidev0077@users.noreply.github.com>
2017-07-29 18:57:56 -0700

    Deleted function compMove()

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -84,34 +84,6 @@ def chooseMove(board, movesList):
         return None
 
 
-def compMove(board, computerLetter):
-    if computerLetter == 'X':
-        playerLetter = 'O'
-    else:
-        playerLetter = 'X'
-
-    for i in range(1, 10):
-        copy = getBoardCopy(board)
-        if isSpaceFree(copy, i):
-            makeMove(copy, computerLetter, i)
-            if isWinner(copy, computerLetter):
-                return i
-
-    for i in range(1, 10):
-        copy = getBoardCopy(board)
-        if isSpaceFree(copy, i):
-            makeMove(copy, playerLetter, i)
-            if isWinner(copy, playerLetter):
-                return i
-
-    move = chooseMove(board, [1, 3, 7, 9])
-    if move != None:
-        return move
-
-    if isSpaceFree(board, 5):
-        return 5
-
-    return chooseMove(board, [2, 4, 6, 8])
 
 
 def boardFull(board):
